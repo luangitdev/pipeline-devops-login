c92b8a3473355642578f36d8d0b92f2eb87c149b
luangitdev <luaninveste@gmail.com>
2025-03-08 17:39:19 -0300

    feat: enviar o arquivo MemcachedConfig.java ao repositório

---
+++ app/src/main/java/com/example/MemcachedConfig.java
@@ -0,0 +1,14 @@
+import net.spy.memcached.MemcachedClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+@Configuration
+public class MemcachedConfig {
+    @Bean
+    public MemcachedClient memcachedClient() throws IOException {
+        return new MemcachedClient(new InetSocketAddress("memcached", 11211));
+    }
+}
